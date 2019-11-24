a87326f26dfb0fab3d02074e707cd6d366ffa1e9
Monique Feitosa <54402465+moniquefeitosa@users.noreply.github.com>
2019-11-24 13:38:06 -0300

    Update programa.s

--- programa.s
+++ programa.s
@@ -1,23 +1,23 @@
 
 .data
 mensagem: .asciiz "Digite um numero\n"     #Cria uma variável string
-numero:	.word 1		                   #Cria uma variável int
+numero: .word 1		                   #Cria uma variável int
 resultado: .word 1                         #Cria uma variável int
 	
-	    .text
+	.text
 main:	li $v0, 4
-	    la $a0, mensagem            #Exibe a mensagem na tela
-	    syscall
+	la $a0, mensagem            #Exibe a mensagem na tela
+	syscall
 	
-	    li $v0, 5	            #Lê um inteiro  	
-	    syscall
+	li $v0, 5	            #Lê um inteiro  	
+        syscall
 
-	    sw $v0, numero              #Salva na variavel o que o usuário digitou
-	    add $t0, $v0, $v0           #Coloca no temporário o dobro do valor
-	    sw $t0, resultado           #Salva na variável resultado o valor do temporário
+	sw $v0, numero              #Salva na variavel o que o usuário digitou
+	add $t0, $v0, $v0           #Coloca no temporário o dobro do valor
+	sw $t0, resultado           #Salva na variável resultado o valor do temporário
 
-	    li $v0, 1
-	    move $a0, $t0               #Imprime na tela o resultado
-	    syscall
+	li $v0, 1
+	move $a0, $t0               #Imprime na tela o resultado
+	syscall
 
-	    jr $ra
+	jr $ra
